ce9290a625f707c422a8ecee476ffb756ce577c0
SSR <someshwar.sangaraju19@gmail.com>
2025-11-12 13:03:59 +0530

    bastion

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "someshwar-devops-dev"
+    bucket = "someshwar-devops"
     key    = "roboshop-dev-bastion"
     region = "us-east-1"
     use_lockfile = true

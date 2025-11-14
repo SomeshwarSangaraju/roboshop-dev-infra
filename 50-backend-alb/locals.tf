904d2c57e5f38577ce18785a31e13366623b9305
SSR <someshwar.sangaraju19@gmail.com>
2025-11-14 20:47:38 +0530

    roboshop

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,7 +1,7 @@
 locals{
     common_suffix_name="${var.project_name}-${var.environment}"
-    backend_alb_sg_id= data.aws_ssm_parameter.backend_alb.id
-    private_subnet_ids= split("," , data.aws_ssm_parameter.public_subnet_ids.value)
+    backend_alb_sg_id= data.aws_ssm_parameter.backend_alb_sg_id.value
+    private_subnet_ids= split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags={
         project_name= "${var.project_name}"
         environment= "${var.environment}"

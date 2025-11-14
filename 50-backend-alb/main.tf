63e9f02e46c33bffd5f5f7ec88ceccf5b7fb12c5
SSR <someshwar.sangaraju19@gmail.com>
2025-11-14 17:27:14 +0530

    backend alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -14,3 +14,32 @@ resource "aws_lb" "backend_alb" {
         }
     )
 }
+
+resource "aws_lb_listener" "backend_alb" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
+
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = "${var.zone_id}"
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}

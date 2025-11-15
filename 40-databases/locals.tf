fc113f927a2a98867b8a6187b36284d2d1f4ae09
SSR <someshwar.sangaraju19@gmail.com>
2025-11-15 09:17:53 +0530

    roboshop

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,8 +1,9 @@
 locals{
     common_suffix_name="${var.project_name}-${var.environment}"
+    ami_id=data.aws_ami.joindevops.id
     bastion_sg_id=data.aws_ssm_parameter.bastion_sg_id.value
     database_subnet_ids=data.aws_ssm_parameter.database_subnet_ids.value
-    ami_id=data.aws_ami.joindevops.id
+    mongodb_sg_id=data.aws_ssm_parameter.mongodb_sg_id.value
     common_tags={
         project_name="${var.project_name}"
         environment="${var.environment}"
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,22 +1,14 @@
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-resource "aws_instance" "bastion" {
+resource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [local.bastion_sg_id]
+  vpc_security_group_ids = [local.mongodb_sg_id]
   subnet_id = local.database_subnet_ids
   
-tags = merge (
-  local.common_tags,
-  {
-      Name = "${local.common_suffix_name}-mongodb" # roboshop-dev-mongodb
-  }
-)
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_suffix_name}-mongodb" # roboshop-dev-mongodb
+    }
+  )
 }

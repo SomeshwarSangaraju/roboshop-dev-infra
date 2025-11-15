f97ed76f532bc859a50318f21aa642856f09a645
SSR <someshwar.sangaraju19@gmail.com>
2025-11-15 17:05:22 +0530

    roboshop

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,8 +2,8 @@ locals{
     common_suffix_name="${var.project_name}-${var.environment}"
     ami_id=data.aws_ami.joindevops.id
     bastion_sg_id=data.aws_ssm_parameter.bastion_sg_id.value
-    database_subnet_ids=data.aws_ssm_parameter.database_subnet_ids.value
     mongodb_sg_id=data.aws_ssm_parameter.mongodb_sg_id.value
+    database_subnet_ids = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
     common_tags={
         project_name="${var.project_name}"
         environment="${var.environment}"

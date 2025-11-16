a09c21f611c1af123388d9b3057a90034ef14d04
SSR <someshwar.sangaraju19@gmail.com>
2025-11-16 16:38:35 +0530

    roboshop

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,10 +1,17 @@
 resource "aws_instance" "bastion" {
-  ami           = local.ami_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [local.bastion_sg_id]
-  subnet_id = local.public_subnet_ids
-  
-   tags = merge (
+    ami           = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.bastion_sg_id]
+    subnet_id = local.public_subnet_ids
+
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3" # or "gp2", depending on your preference
+    }
+
+    user_data = file("bastion.sh")
+        
+    tags = merge (
         local.common_tags,
         {
             Name = "${var.project_name}-${var.environment}-bastion"

489fec57df1aa6a8119ee27a0e368e37f6dcabec
SSR <someshwar.sangaraju19@gmail.com>
2025-11-17 17:37:29 +0530

    roboshop

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,8 @@
 locals{
     ami_id=data.aws_ami.joindevops.id
     common_suffix_name= "${var.project_name}-${var.environment}"
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     common_tags={
         project_name="${var.environment}"
         environment="${var.environment}"
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,8 +1,40 @@
 resource "aws_instance" "catalogue" {
   ami           = local.ami_id 
   instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  subnet_id = local.private_subnet_ids
 
-  tags = {
-    Name = "HelloWorld"
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_suffix_name}-catalogue" # roboshop-dev-mongodb
+    }
+  )
+}
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+  }
+
+  # terraform copies this file to catalogue server
+  provisioner "file" {
+    source = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/catalogue.sh",
+        # "sudo sh /tmp/catalogue.sh"
+        "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
+    ]
   }
 }

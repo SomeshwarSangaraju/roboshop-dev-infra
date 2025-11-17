333af4bde5f6da91b1ea604febdd155db92c6e49
SSR <someshwar.sangaraju19@gmail.com>
2025-11-17 17:06:04 +0530

    roboshop

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -1,15 +1,7 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+variable "environment"{
+    default="dev"
+}
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+variable "project_name"{
+    default= "roboshop"
 }

c4b2112c89230a0e509ba64a7785ddadfec6047a
SSR <someshwar.sangaraju19@gmail.com>
2025-11-18 16:09:44 +0530

    roboshop

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -2,18 +2,15 @@ resource "aws_lb" "frontend_alb" {
   name               = "${local.common_suffix_name}-backend-alb"  #roboshop-dev-backend-alb
   internal           = false
   load_balancer_type = "application"
-  security_groups    = local.frontend_alb_sg_id
-  subnets            = [local.public_subnet_ids]
+  security_groups    = [local.frontend_alb_sg_id]
+  subnets            = local.public_subnet_ids
 
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
-  tags = {
-    Environment = "production"
-  }
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_suffix_name}-frontend-alb"
+    }
+  )
 }

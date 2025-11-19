56d7bfe6f930b0a8230e0ba427edbad441a2020a
SSR <someshwar.sangaraju19@gmail.com>
2025-11-19 15:35:54 +0530

    roboshop

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,8 @@ locals{
     ami_id=data.aws_ami.joindevops.id
     common_suffix_name= "${var.project_name}-${var.environment}"
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     vpc_id = data.aws_ssm_parameter.vpc_id.id
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     common_tags={

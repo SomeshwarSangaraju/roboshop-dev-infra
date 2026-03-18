38f36e022f856d123d47d335ed2a0ca7003a06e8
SSR <someshwar.sangaraju19@gmail.com>
2026-03-18 17:37:02 +0530

    roboshop infra

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "frontend_alb" {
-  name               = "${local.common_suffix_name}-backend-alb"  #roboshop-dev-backend-alb
+  name               = "${local.common_suffix_name}-frontend-alb"  #roboshop-dev-frontend-alb
   internal           = false
   load_balancer_type = "application"
   security_groups    = [local.frontend_alb_sg_id]
